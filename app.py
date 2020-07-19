364826ea16d415c69fdc374520c4c575fae198a2
Utku Cuhadaroglu <utku.cuhadaroglu@gmail.com>
2020-07-19 03:14:58 +0300

    Attemp to connect to mongodb atlas

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
+import os
+
 from flask import Flask
+from pymongo import MongoClient
 
 app = Flask(__name__)
-
-
+client = MongoClient(os.environ['CUSTOMCONNSTR_MongoDB'])
+db = client.Geoart
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    test_dic = db.test.find_one(dict(name="utku"))
+    if test_dic is not None:
+        return str(test_dic["name2"])
+    else:
+        return 'Hello World!'
 
 
 if __name__ == '__main__':
