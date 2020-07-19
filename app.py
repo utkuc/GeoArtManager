b951a3083719e9e371df1bd8f372d3592b1d8b82
Utku Cuhadaroglu <utku.cuhadaroglu@gmail.com>
2020-07-19 04:15:18 +0300

    Added default layout and index pages for view.

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 from pymongo import MongoClient
 
 app = Flask(__name__)
+
+
 client = MongoClient(os.environ['CUSTOMCONNSTR_MongoDB'])
 db = client.Geoart
 @app.route('/')
 def hello_world():
-    test_dic = db.test.find_one(dict(name="utku"))
-    if test_dic is not None:
-        return str(test_dic["name2"])
-    else:
-        return 'Hello World!'
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
